8f40f7e0bfdfc744fc45a7578253b336507d146b
Robin Guan <hello@robinguan.com>
2019-01-03 16:08:31 -0800

    updated people and stacks using view and collection

--- people_and_stacks/js/views.js
+++ people_and_stacks/js/views.js
@@ -9,35 +9,59 @@ var StackModel = Backbone.Model.extend({
   }
 });
 
+var PeopleList = Backbone.Collection.extend({
+  model: PersonModel
+});
+var StacksList = Backbone.Collection.extend({
+  model:StackModel
+});
+
 var barry = new PersonModel({name: "Barry"});
 var larry = new PersonModel({name: "Larry"});
 var terry = new PersonModel({name: "Terry"});
 var jerry = new PersonModel({name: "Jerry"});
 var harry = new PersonModel({name: "Harry"});
+var myPeople = new PeopleList([barry, larry, terry, jerry, harry]);
 
 var ruby = new StackModel({name: "Ruby"});
 var ios = new StackModel({name: "IOS"});
 var lamp = new StackModel({name: "LAMP"});
 var mean = new StackModel({name: "MEAN"});
 var python = new StackModel({name: "Python"});
+var myStacks = new StacksList([ruby, ios, lamp, mean, python]);
+
+var myPersonTemplate = "<div class='redbox'><%= name %></div>"
+var myStackTemplate = "<div class='greenbox'><%= name %></div>"
 
 var PersonView = Backbone.View.extend({
   tagName: 'li',
-  template: _.template("<div class='redbox'><%= name %></div>"),
+  template: _.template(myPersonTemplate),
   initialize: function(){
     this.render();
   },
   render: function(){
     this.$el.html(this.template(this.model.toJSON()));
-    $('.people').append(this.$el);
     return this;
   }
 });
-var personView1 = new PersonView({model: barry});
-var personView2 = new PersonView({model: larry});
-var personView3 = new PersonView({model: terry});
-var personView4 = new PersonView({model: jerry});
-var personView5 = new PersonView({model: harry});
+
+//collection view
+var PeopleListView = Backbone.View.extend({
+  el: '#people',
+  render: function(){
+    this.$el.empty();
+    this.collection.each(function(person){
+      var personview = new PersonView({model:person});
+      this.$el.append(personview.render().$el);
+    }, this);
+  },
+  initialize: function(){
+   this.render();
+  }
+ });
+
+var myPeopleList = new PeopleListView({collection: myPeople});
+
 
 var StackView = Backbone.View.extend({
   tagName: 'li',
@@ -47,12 +71,21 @@ var StackView = Backbone.View.extend({
   },
   render: function(){
     this.$el.html(this.template(this.model.toJSON()));
-    $('.stacks').append(this.$el);
     return this;
   }
 });
-var StackView1 = new StackView({model: ruby});
-var StackView2 = new StackView({model: ios});
-var StackView3 = new StackView({model: lamp});
-var StackView4 = new StackView({model: mean});
-var StackView5 = new StackView({model: python});
+
+var StacksListView = Backbone.View.extend({
+  el: '#stacks',
+  render: function(){
+    this.$el.empty();
+    this.collection.each(function(stack){
+      var stackview = new StackView({model:stack});
+      this.$el.append(stackview.render().$el);
+    }, this);
+  },
+  initialize: function(){
+    this.render();
+  }
+});
+var myStacksList = new StacksListView({collection: myStacks});
